d3fb6f92baf9bea857008f3a1e01edf5ba48094b
Bryce Murray <brycepmurray711@gmail.com>
2018-03-27 15:01:49 -0600

    some game.cs set up...need to finish player things in here for player.cs

--- Project/Game.cs
+++ Project/Game.cs
@@ -5,17 +5,57 @@ namespace CastleGrimtol.Project
 {
     public class Game : IGame
     {
-        public Room CurrentRoom { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Player CurrentPlayer { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Room CurrentRoom { get; set; }
+        public Player CurrentPlayer { get; set; }
+        public List<Room> Rooms { get; set; }
+        public Boolean Playing { get; set; }
 
         public void Reset()
         {
-            throw new NotImplementedException();
+            Playing = true; ;
         }
 
         public void Setup()
         {
-            throw new NotImplementedException();
+            Console.Clear();
+            CurrentPlayer = new Player();
+            if (CurrentPlayer.PlayerName == "D$" || CurrentPlayer.PlayerName == "DMONEY" || CurrentPlayer.PlayerName == "DARRYL")
+            {
+                Win();
+                // DARRYL();
+            }
+        }
+        // public void DARRYL()
+        // {
+        //     Console.WriteLine($@"
+        //        You are Walking in the woods, 
+        //        Theres no one around and your phone is dead, 
+        //        out of the corner of your eye, you spot him....
+        //        (whisper)Shia LaBeouf...
+        //        ");
+        //     Console.WriteLine($@"
+        //        As if in response, the ground benieth you
+        //        echos your pressence back towards your pursuer...
+        //        In a rumbling whisper that would shake even the 
+        //        strongest of men or women to the core you hear.....
+        //        DDDDDDD MOOONNNEEEYYY...
+        //     ");
+        //     Console.WriteLine($@"
+        //        Your Pursuer Stops Dead in his Tracks,
+        //        Close enough to see that it is none other
+        //         than Hollywood superstar Shia LaBeouf...
+        //        You see absolute fear in his eyes and sweat 
+        //        drip down his face....He Flees in terror
+        //        knowing that he stands no chance against 
+        //        the Being of Absolute Power D$!
+        //     ");
+        //     CurrentPlayer.Score++;
+        //     DARRYL();
+        // }
+        public void Win()
+        {
+            Console.Clear();
+            
         }
 
         public void UseItem(string itemName)
--- Project/Player.cs
+++ Project/Player.cs
@@ -1,15 +1,25 @@
+using System;
 using System.Collections.Generic;
 
 namespace CastleGrimtol.Project
 {
     public class Player : IPlayer
     {
+        public string PlayerName;
+        public List<Item> Inventory { get; set; }
+        public int Score { get; set; }
+
         public Player()
         {
-            
+            PlayerName = NamePlayer();
+            Score = 0;
+            Inventory = new List<Item>();
         }
-
-        public int Score { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        public List<Item> Inventory { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string NamePlayer()
+        {
+        Console.WriteLine("What would you like to be known by?");
+        string name = Console.ReadLine();
+        
+        }
     }
 }
